4925cc108c3f8878d1c1645981ad8724b88fd68d
MedClch <chlouchi.med@gmail.com>
2023-11-01 10:04:38 +0100

    xxx

--- src/main/java/com/example/projet/Model/Admin.java
+++ src/main/java/com/example/projet/Model/Admin.java
@@ -3,13 +3,21 @@ package com.example.projet.Model;
 public class Admin extends Utilisateur {
     private static Admin ADMIN = new Admin();
 
-    public Admin() {
+    private Admin() {
         super();
         setFullname("Admin");
         setPassword("12345");
         setUsername("admin");
         setEmail("admin@admin.com");
     }
+
+//    public Admin() {
+//        super();
+//        setFullname("Admin");
+//        setPassword("12345");
+//        setUsername("admin");
+//        setEmail("admin@admin.com");
+//    }
 
     public static Admin getInstance() {
         return ADMIN;
