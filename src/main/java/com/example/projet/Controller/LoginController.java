6afa3547ea6cec5b847f0f2826d875f4bc5f5100
MedClch <chlouchi.med@gmail.com>
2023-11-01 12:45:16 +0100

    xxx

--- src/main/java/com/example/projet/Controller/LoginController.java
+++ src/main/java/com/example/projet/Controller/LoginController.java
@@ -5,18 +5,16 @@ import com.example.projet.Services.iServiceUtilisateur;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+//@RequestMapping("/login")
 @CrossOrigin("http://localhost:3000")
 public class LoginController {
     @Autowired
     private iServiceUtilisateur utilisateurService;
 
-    @PostMapping("/login")
+    @GetMapping("/login")
     public ResponseEntity<Utilisateur> loginUser(@RequestParam String username, @RequestParam String password) {
         Utilisateur loggedInUser = utilisateurService.loginUser(username, password);
         if (loggedInUser != null)
